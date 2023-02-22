ca9bd0fce8280ac4a48700597e6cd02b552e1070
huoyuqi <huoyuqi.hyq@antgroup.com>
2023-02-22 10:46:16 +0800

    update pom

--- src/main/java/com/alipay/autotuneservice/base/wechat/impl/WechatProviderImpl.java
+++ src/main/java/com/alipay/autotuneservice/base/wechat/impl/WechatProviderImpl.java
@@ -17,10 +17,6 @@
 package com.alipay.autotuneservice.base.wechat.impl;
 
 import com.alipay.autotuneservice.base.wechat.NotifyClientProvider;
-import com.alipay.saascloud.driver.notify.qywechat.api.DefaultWorkWechatClient;
-import com.alipay.saascloud.driver.notify.qywechat.api.WorkWechatClient;
-import com.alipay.saascloud.driver.notify.qywechat.api.request.OapiRobotSendRequest;
-import com.alipay.saascloud.driver.notify.qywechat.api.response.WorkWechatResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -47,31 +43,6 @@ public class WechatProviderImpl implements NotifyClientProvider {
     @Override
     public boolean sendMessageWebhook(String webHookUrl, String messageType, String title, String content) {
 
-        WorkWechatClient client = new DefaultWorkWechatClient(webHookUrl);
-        OapiRobotSendRequest request = new OapiRobotSendRequest();
-        request.setMsgtype(messageType);
-        // 判断发送webhook 消息的类型，目前暂时仅支持text/markdown类型的消息
-        switch (messageType) {
-            case "text":
-                OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
-                text.setContent(content);
-                request.setText(text);
-                break;
-            case "markdown":
-                OapiRobotSendRequest.MarkDown markdown = new OapiRobotSendRequest.MarkDown();
-                markdown.setTitle(title);
-                markdown.setContent(content);
-                request.setMarkdown(markdown);
-                break;
-            default:
-                break;
-        }
-
-        try {
-            WorkWechatResponse response = client.execute(request);
-            return response.getErrcode() == 0;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return true;
     }
 }
